f858a15a4edf47449db5ac133529d5f50f1da632
polaro23daw2 <15586147.clot@fje.edu>
2024-05-10 08:59:55 +0200

    dashboard solucionat

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ url('clients') }}">Clients:
+                        Visualitza, actualitza i esborra registres<a />
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ url('clients/create') }}">Crea un nou Client<a />
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ url('apart') }}">Apartament: Visualitza, actualitza i esborra registres<a />
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ url('apart/create') }}">Apartament: Crea un nou Apartament<a />
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ url('lloga') }}">Lloga: Visualitza, actualitza i esborra registres<a />
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ url('lloga/create') }}">Lloga: Crea un nou Apartament<a />
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
                     <a href="{{ url('trebs') }}">Treballadors: Visualitza, actualitza i esborra registres<a />
                 </div>
                 <div class="p-6 text-gray-900">
